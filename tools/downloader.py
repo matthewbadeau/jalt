05808437ac613500f2f8eb9111b9c185c3dab058
Benjamin Murden <ben.murden@gmail.com>
2017-12-10 13:39:33 +0900

    Add verbose output. PEP8.

--- tools/downloader.py
+++ tools/downloader.py
@@ -1,7 +1,9 @@
 import argparse
 import requests
 
-parser = argparse.ArgumentParser(description="Sequentially collect legal documents from www.japaneselawtranslation.go.jp.")
+parser = argparse.ArgumentParser(
+    description="Sequentially collect legal documents from www.japaneselawtranslation.go.jp."
+)
 parser.add_argument("-s", "--start", help="The ID at which to start.", type=int, default=5)
 parser.add_argument("-e", "--end", help="The ID at which to finish.", type=int, default=6)
 parser.add_argument(
@@ -9,22 +11,31 @@ parser.add_argument(
     "--format",
     help="Format to retreive. See https://github.com/matthewbadeau/jalt/wiki/Notes for details.",
     type=str,
-    default='01' 
+    default='01'
 )
 parser.add_argument("-v", "--verbose", help="Show more information.", action="store_true")
 args = parser.parse_args()
 url = 'http://www.japaneselawtranslation.go.jp/law/detail_download/'
 
 for x in range(args.start, args.end):
-    params = { 'ff': args.format, 'id': x }
+    params = {'ff': args.format, 'id': x}
     filename = params['ff'] + "_id_" + str(x) + ".xml"
     response = requests.get(url, allow_redirects=True, params=params)
 
     if response.status_code == 200:
+        if args.verbose:
+            print("{id} exists. Writing to {filename}.".format(
+                id=x,
+                filename=filename
+            ))
         with open(filename, 'wb') as fd:
             for chunk in response.iter_content(chunk_size=128):
                 fd.write(chunk)
     elif response.status_code == 404:
+        if args.verbose:
+            print("{id} does not exist. Continuing.".format(
+                id=x
+            ))
         continue
     else:
         print('Server responded with {status} for id {id}'.format(
